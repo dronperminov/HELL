73a563d83353dd77cf7aec75433a1b62d651bdff
dronperminov <dronperminov@gmail.com>
2023-02-02 00:11:30 +0300

    add tabs header control

--- web/js/swipe_tabs.js
+++ web/js/swipe_tabs.js
@@ -1,5 +1,6 @@
-function SwipeTabs(container) {
+function SwipeTabs(container, header) {
     this.container = container
+    this.header = header
     this.tabs = container.children.length
     this.tab = 0
 
@@ -8,12 +9,24 @@ function SwipeTabs(container) {
     this.container.addEventListener("touchend", (e) => this.TouchEnd(e))
     this.container.addEventListener("transitionend", () => this.TransitionEnd())
 
+    for (let i = 0; i < this.tabs; i++)
+        header.children[i].addEventListener("click", () => this.ChangeTab(i))
+
     this.UpdateParams()
+}
+
+SwipeTabs.prototype.UpdateHeader = function(tab) {
+    for (let i = 0; i < this.tabs; i++)
+        this.header.children[i].classList.remove("active")
+
+    this.header.children[tab].classList.add("active")
+    this.header.scrollLeft = this.header.children[tab].offsetLeft - 10
 }
 
 SwipeTabs.prototype.UpdateParams = function() {
     this.container.style.transform = `translateX(${-this.tab * this.container.clientWidth}px)`
     this.container.style.height = `${this.container.children[this.tab].clientHeight}px`
+    this.UpdateHeader(this.tab)
 }
 
 SwipeTabs.prototype.TouchStart = function(e) {
@@ -32,26 +45,34 @@ SwipeTabs.prototype.TouchMove = function(e) {
     e.preventDefault()
 
     this.isStarted = true
-    this.offset = this.tab * this.container.clientWidth - deltaX
+    let offset = this.tab * this.container.clientWidth - deltaX
 
-    if (this.offset < 0)
+    if (offset < 0)
         this.startX += deltaX
 
-    this.offset = Math.max(0, Math.min(this.container.clientWidth * (this.tabs - 1), this.offset))
-    this.container.style.transform = `translateX(${-this.offset}px)`
+    offset = Math.max(0, Math.min(this.container.clientWidth * (this.tabs - 1), offset))
+    this.part = Math.min(offset / this.container.clientWidth, this.tabs - 1)
+
+    this.container.style.transform = `translateX(${-offset}px)`
+
+    let tab = Math.round(this.part) == this.tab ? this.tab : this.tab - Math.sign(deltaX)
     this.container.style.transition = 'height 0.3s'
-    this.container.style.height = `${this.container.children[Math.max(0, Math.min(this.tabs - 1, this.tab - Math.sign(deltaX)))].clientHeight}px`
+    this.container.style.height = `${this.container.children[Math.max(0, Math.min(this.tabs - 1, tab))].clientHeight}px`
+
+    this.UpdateHeader(Math.round(this.part))
 }
 
 SwipeTabs.prototype.TouchEnd = function(e) {
-    let part = Math.min(this.offset / this.container.clientWidth, this.tabs - 1)
-
     this.isStarted = false
-    this.tab = Math.round(part)
-    this.container.style.transition = 'transform 0.15s, height 0.15s'
-    this.UpdateParams()
+    this.ChangeTab(Math.round(this.part))
 }
 
 SwipeTabs.prototype.TransitionEnd = function() {
     this.container.style.transition = null
+}
+
+SwipeTabs.prototype.ChangeTab = function(tab) {
+    this.tab = tab
+    this.container.style.transition = 'transform 0.15s, height 0.15s'
+    this.UpdateParams()
 }
