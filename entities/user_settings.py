b83ad99646d7f7aac44bc46161fa899956677df0
dronperminov <dronperminov@gmail.com>
2023-03-01 18:25:35 +0300

    add calories showing

--- entities/user_settings.py
+++ entities/user_settings.py
@@ -15,6 +15,7 @@ class UserSettings:
     show_frequent_all: bool
     show_recent: bool
     show_recent_all: bool
+    show_calories: bool
 
     @classmethod
     def from_dict(cls, data: dict) -> "UserSettings":
@@ -25,8 +26,9 @@ class UserSettings:
         show_frequent_all = data.get("show_frequent_all", True)
         show_recent = data.get("show_recent", True)
         show_recent_all = data.get("show_recent_all", True)
+        show_calories = data.get("show_calories", True)
 
-        return cls(data["user_id"], theme, limits, add_limits, show_frequent, show_frequent_all, show_recent, show_recent_all)
+        return cls(data["user_id"], theme, limits, add_limits, show_frequent, show_frequent_all, show_recent, show_recent_all, show_calories)
 
     def to_dict(self) -> dict:
         return {
@@ -37,7 +39,8 @@ class UserSettings:
             "show_frequent": self.show_frequent,
             "show_frequent_all": self.show_frequent_all,
             "show_recent": self.show_recent,
-            "show_recent_all": self.show_recent_all
+            "show_recent_all": self.show_recent_all,
+            "show_calories": self.show_calories
         }
 
     def limits_to_dict(self):
